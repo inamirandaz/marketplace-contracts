b5d0ecd120e1de36714d5a6b8a98343d240f865c
Ina Miranda <ina@zilliqa.com>
2022-11-08 19:54:51 +0800

    Fixed error code, added msw

--- scripts/fixed_price_signed/setup.sh
+++ scripts/fixed_price_signed/setup.sh
@@ -78,9 +78,32 @@ node contract.js fulfillorder $fp_addr $zrc6_addr $token_id $SELL_SIDE $BUYER_WA
 node contract.js fulfillorder $fp_addr $zrc6_addr $token_id $SELL_SIDE $BUYER_WALLET 0
 cd -
 
+#re-enable the mode for testing later
+node contract.js regpubkey $fp_addr $PUBKEY 
+
+echo "Deploying multisig"
+cd $TOOL_PATH
+MSW_FILE="../../contracts/msw.scilla"
+MSW_INIT="init/init-msw.json"
+ret=`zli contract deploy -c $MSW_FILE -i $MSW_INIT -p 2000000000 -l 40000`
+msw_addr="0x`echo "$ret" | grep "contract address" | awk '{print $(NF)}'`"
+cd -
+
+
+
+echo "Transferring ownership to multisig"
+
+zli contract call -a $fp_addr -t SetContractOwnershipRecipient -r "[{\"vname\":\"to\",\"type\":\"ByStr20\",\"value\":\"$msw_addr\"}]"
+zli contract call -a $msw_addr -t SubmitAcceptContractOwnershipTransaction -r "[{\"vname\":\"contract_address\",\"type\":\"ByStr20\",\"value\":\"$fp_addr\"}]"
+zli contract call -a $msw_addr -t ExecuteTransaction -r "[{\"vname\":\"transaction_id\",\"type\":\"Uint32\",\"value\":\"0\"}]"
+
+echo "Setting"
+zli contract call -a$msw_addr -t SubmitDisableSignedOrderTransaction -r "[{\"vname\":\"contract_address\",\"type\":\"ByStr20\",\"value\":\"$fp_addr\"}]"
+zli contract call -a $msw_addr -t ExecuteTransaction -r "[{\"vname\":\"transaction_id\",\"type\":\"Uint32\",\"value\":\"1\"}]"
 
 
 echo "Contracts"
 echo "zrc6=$zrc6_addr"
 echo "collection=$collection_addr"
 echo "fp=$fp_addr"
+echo "msw=$msw_addr"
